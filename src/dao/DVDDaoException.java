cabbd488e74c73b3632c63aa56f111dc1aab3585
yusraalnas <45268417+yusraalnas@users.noreply.github.com>
2022-06-26 23:38:47 +0100

    Add files via upload

---
+++ src/dao/DVDDaoException.java
@@ -0,0 +1,15 @@
+package dao;
+
+public class DVDDaoException extends Exception{
+
+
+    public DVDDaoException(String message) {
+        super(message);
+    }
+
+    public DVDDaoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
+
